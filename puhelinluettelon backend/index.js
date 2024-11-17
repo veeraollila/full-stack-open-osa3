b5d3cbf4eced907ca9818d1d852f69aa105ee336
veeraollila <veera.ollila@live.fi>
2024-11-17 22:25:38 +0200

    puhelinluettelon backend

--- puhelinluettelon backend/index.js
+++ puhelinluettelon backend/index.js
@@ -1,8 +1,12 @@
 const express = require('express')
+const morgan = require("morgan")
+const cors = require("cors")
 const app = express()
 
 app.use(express.json())
 app.use(morgan)
+app.use(cors())
+app.use(express.static('dist'))
 
 let persons = [
     {
@@ -90,7 +94,7 @@ app.delete("/api/persons/:id", (req, res) => {
     res.status(204).end()
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
