a228d4404fa782d2cf14f8bf98e79a790ad8e65a
veeraollila <veera.ollila@live.fi>
2024-12-01 04:50:47 +0200

    puhelinluettelon backend step3

--- index.js
+++ index.js
@@ -4,22 +4,22 @@ app.use(express.json())
 
 let persons = [
     {
-        id: 1,
+        id: '1',
         name: 'Arto Hellas',
         number: '040-123456'
     },
     {
-        id: 2,
+        id: '2',
         name: 'Ada Lovelace',
         number: '39-44-5323523'
     },
     {
-        id: 3,
+        id: '3',
         name: 'Dan Abramov',
         number: '12-43-234345'
     },
     {
-        id: 4,
+        id: '4',
         name: 'Mary Poppendieck',
         number: '39-23-6423122'
     }
@@ -39,6 +39,17 @@ app.get('/info', (req, res) => {
         <p>Phonebook has info for ${persons.length} people</p>
         <p>${date}</p>
     `)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+    const id = req.params.id
+    const person = persons.find((person) => person.id === id)
+    
+    if (person) {
+        res.json(person)
+    } else {
+        res.status(404).end()
+    }
 })
 
 const PORT = 3001
