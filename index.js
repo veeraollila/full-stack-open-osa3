8f2a53eb16a204b54fcc8daae68ef7ccf9d4163d
veeraollila <veera.ollila@live.fi>
2024-12-13 20:42:08 +0200

    väärä portti

--- index.js
+++ index.js
@@ -142,7 +142,7 @@ app.use((error, req, res, next) => {
 
 
 
-const PORT = 3002
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
